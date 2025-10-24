8e52c2ca50fc23f29374602373937f012bdad089
egemenkeles <egemenkoray05@gmail.com>
2025-10-24 17:17:57 +0300

    Login Screen With React

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './LoginForm.css';
+import { FaUser } from "react-icons/fa";
+import { IoLockClosed } from "react-icons/io5";
+
+const LoginForm = () => {
+    return (
+        <div className='wrapper'>
+            <form action="">
+                <h1>Giriş</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Kullanıcı Adı " required/>
+                    <FaUser className='icon' />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Şifre" required/>
+                    <IoLockClosed className='icon' />
+                </div>
+                <div className="remember-forgot">
+                    <label><input type="checkbox"/>Beni Hatırla </label>
+                    <a href="#">Şifremi Unuttum</a>
+                </div>
+
+                <button type="submit">Giriş</button>
+
+                <div className="register-link">
+                    <p>Hesabın mı yok? <a href="#">Kaydol.</a></p>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
